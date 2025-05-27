3b3a563f68987cf09e969e2af103e7198760275e
sandesh <sandeshbhatta218@gmail.com>
2025-05-27 08:51:52 +0545

    Basics of pivotControls.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,8 @@
-import { OrbitControls, TransformControls } from "@react-three/drei";
+import {
+  PivotControls,
+  OrbitControls,
+  TransformControls,
+} from "@react-three/drei";
 import { useRef } from "react";
 
 export default function Experience() {
@@ -9,17 +13,25 @@ export default function Experience() {
 
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
-
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={3}
+        axisColors={["#9381ff", "#ff4d6d", "#7ae582"]}
+        scale={2}
+        fixed={true}
+      >
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+      </PivotControls>
 
       <mesh ref={cube} position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
-      <TransformControls object={cube} mode="scale" />
+      <TransformControls object={cube} mode="translate" />
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
